fdd306af3108cb088ad269543550bc0d2d3401a7
samru <samru@LAPTOP-LFQGVS1J>
2023-03-20 13:07:27 +0530

    first commit

---
+++ trainingcodes/test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package trainingcodes;
+
+/**
+ * @author samru
+ *
+ */
+public class test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
